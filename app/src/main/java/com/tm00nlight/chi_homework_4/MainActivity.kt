dbcaaa702713d916cfba69e88f5149b16b091d32
tm00nlight <95237741+tm00nlight@users.noreply.github.com>
2022-10-06 14:56:14 +0300

    Task 1 done

--- app/src/main/java/com/tm00nlight/chi_homework_4/MainActivity.kt
+++ app/src/main/java/com/tm00nlight/chi_homework_4/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.tm00nlight.chi_homework_4
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var playMusicService: Intent
+    private val receiver = TrackProgressReceiver()
+    private val filter = IntentFilter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playMusicService =Intent(applicationContext, PlayMusicService::class.java)
+
+        val playButton = findViewById<Button>(R.id.playButton)
+        val stopButton = findViewById<Button>(R.id.stopButton)
+
+        playButton.setOnClickListener {
+            startService(playMusicService)
+        }
+
+        stopButton.setOnClickListener {
+            stopService(playMusicService)
+        }
+
+        filter.addAction("TRACK_PROGRESS")
+        registerReceiver(receiver, filter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(receiver)
+    }
+
+    private fun updateProgressBar(progress: Int) {
+        val progressBar = findViewById<ProgressBar>(R.id.trackProgress)
+        progressBar.setProgress(progress, false)
+    }
+
+    inner class TrackProgressReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val progress: Int
+            val action = intent?.action
+            if(action.equals("TRACK_PROGRESS")){
+                progress = intent?.getIntExtra("progress", 0) ?: 0
+                Log.d("BROADCAST", "received $progress")
+                updateProgressBar(progress)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/tm00nlight/chi_homework_4/PlayMusicService.kt
@@ -0,0 +1,91 @@
+package com.tm00nlight.chi_homework_4
+
+import android.app.*
+import android.content.Intent
+import android.content.IntentFilter
+import android.media.MediaPlayer
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+const val TAG = "PlayMusicService"
+class PlayMusicService : Service() {
+    var progress = 0
+    //private val receiver = MainActivity.TrackProgressReceiver()
+    private val filter = IntentFilter()
+    private val mediaPlayer: MediaPlayer by lazy { MediaPlayer.create(this, R.raw.hear_me) }
+
+    override fun onCreate() {
+        filter.addAction("TRACK_PROGRESS")
+        playMusic()
+        showNotification()
+        Log.d(TAG, "Service started")
+    }
+
+    override fun onDestroy() {
+        stopMusic()
+        //unregisterReceiver(receiver)
+        Log.d(TAG, "Service stopped")
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    private fun playMusic() {
+        mediaPlayer.start()
+        val thread = Thread(Runnable {
+            while (mediaPlayer.isPlaying) {
+                progress = 100 * mediaPlayer.currentPosition / mediaPlayer.duration
+                Thread.sleep(1000)
+                Log.d("THREAD", progress.toString())
+                Intent().also { intent ->
+                    intent.action = "TRACK_PROGRESS"
+                    intent.putExtra("progress", progress)
+                    sendBroadcast(intent)
+                }
+            }
+        })
+        thread.start()
+    }
+
+    private fun stopMusic() {
+        mediaPlayer.stop()
+    }
+
+    private fun showNotification() {
+        val channelId = "audio_channel"
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Audio Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val pendingIntent: PendingIntent =
+            Intent(this, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(
+                    this,
+                    0,
+                    notificationIntent,
+                    PendingIntent.FLAG_MUTABLE
+                )
+            }
+
+        val notification: Notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Playing song")
+            .setContentText("Imagine Dragons - Hear Me")
+            .setSmallIcon(R.drawable.hear_me)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        startForeground(500, notification)
+    }
+}
